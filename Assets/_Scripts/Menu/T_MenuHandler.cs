a88238366dac316523d5434d91f8368efa4b9420
Théo TURPIN <theo.turpin@epitech.eu>
2025-02-04 12:11:13 +0100

    feat: finish menu

--- Assets/_Scripts/Menu/T_MenuHandler.cs
+++ Assets/_Scripts/Menu/T_MenuHandler.cs
@@ -1,41 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class T_MenuHandler : MonoBehaviour
 {
-    public string levelOne;
-    public string levelTwo;
-    public string levelThree;
-    public GameObject creditsPanel;
+    [Header("UI")]
+    [SerializeField] private GameObject creditsPanel;
     
+    [Header("Levels")]
+    [SerializeField] private string levelOne;
+    [SerializeField] private string levelTwo;
+    [SerializeField] private string levelThree;
+    
+    [Header("Audio")]
+    private AudioSource _audioSource;
+    [SerializeField] private AudioClip buttonSound;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     // Start the game
     public void StartLevelOne()
     {
+        _audioSource.PlayOneShot(buttonSound);
         UnityEngine.SceneManagement.SceneManager.LoadScene(levelOne);
     }
     public void StartLevelTwo()
     {
+        _audioSource.PlayOneShot(buttonSound);
         UnityEngine.SceneManagement.SceneManager.LoadScene(levelTwo);
     }
     public void StartLevelThree()
     {
+        _audioSource.PlayOneShot(buttonSound);
         UnityEngine.SceneManagement.SceneManager.LoadScene(levelThree);
     }
     
     // Quit the game
     public void QuitGame()
     {
+        _audioSource.PlayOneShot(buttonSound);
         Application.Quit();
     }
     
     // Crédits
     public void ShowCredits()
     {
+        _audioSource.PlayOneShot(buttonSound);
         creditsPanel.SetActive(true);
     }
     public void CloseCredits()
     {
+        _audioSource.PlayOneShot(buttonSound);
         creditsPanel.SetActive(false);
     }
 }
---
+++ Assets/_Scripts/Score/T_ScoreHandlerMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_ScoreHandlerMenu : MonoBehaviour
+{
+    [Header("LevelOne")]
+    [SerializeField] private GameObject[] noStarsLevelOne;
+    [SerializeField] private GameObject[] starsLevelOne;
+
+    [Header("LevelTwo")]
+    [SerializeField] private GameObject[] noStarsLevelTwo;
+    [SerializeField] private GameObject[] starsLevelTwo;
+
+    [Header("LevelThree")]
+    [SerializeField] private GameObject[] noStarsLevelThree;
+    [SerializeField] private GameObject[] starsLevelThree;
+
+    private void Start()
+    {
+        InitializeStars(noStarsLevelOne, starsLevelOne);
+        InitializeStars(noStarsLevelTwo, starsLevelTwo);
+        InitializeStars(noStarsLevelThree, starsLevelThree);
+
+        SetActiveStars(PlayerPrefs.GetInt("LevelOneScore"), noStarsLevelOne, starsLevelOne);
+        SetActiveStars(PlayerPrefs.GetInt("LevelTwoScore"), noStarsLevelTwo, starsLevelTwo);
+        SetActiveStars(PlayerPrefs.GetInt("LevelThreeScore"), noStarsLevelThree, starsLevelThree);
+    }
+
+    private void InitializeStars(GameObject[] noStars, GameObject[] stars)
+    {
+        foreach (var star in stars)
+        {
+            star.SetActive(false);
+        }
+    }
+
+    private void SetActiveStars(int score, GameObject[] noStars, GameObject[] stars)
+    {
+        for (int i = 0; i < noStars.Length; i++)
+        {
+            noStars[i].SetActive(i >= score);
+        }
+
+        for (int i = 0; i < score; i++)
+        {
+            stars[i].SetActive(true);
+        }
+    }
+}
